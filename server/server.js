1b5adac16dcec364193e01fcf8e3f31635854117
Sh4rp Fro5t <sh4rp.fro5t@gmail.com>
2015-10-28 17:18:04 +0100

    first experimentation of rest API with MongoDB

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -14,38 +16,111 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-
     // Pass to next layer of middleware
     next();
 });
 
+mongoose.connect('mongodb://localhost:27017/Domodi'); // connect to our database
 
 var port = process.env.PORT || 8080;        // set our port
+
+var User = require('./app/models/user');
+
+
 
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to Domodi api!' });   
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Domodi api called with params: ' + req.params);
+    next(); // make sure we go to the next routes and don't stop here
 });
 
-// more routes for our API will happen here
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'Domodi API is alive !' });   
+});
+
+
+//--------------------------------------------------------------------------------
+// Route to get all users or to post a new user
+router.route('/users')
+
+    // POST : create a user (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+        
+        var user = new User();      // create a new instance of the Bear model
+        user.name = req.body.name;  // set the bears name (comes from the request)
+        user.firstName = req.body.firstName;
+
+        // save the bear and check for errors
+        user.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'User ' + user.name + ' ; ' + user.firstName + ' created' });
+        });
+        
+    })
+    // GET : return all the users
+    .get(function(req, res) {
+        User.find(function(err, users) {
+            if (err)
+                res.send(err);
+
+            res.json(users);
+        });
+    });
+
+//--------------------------------------------------------------------------------
+router.route('/users/:user_id')
+	.get(function(req, res) {
+        User.findById(req.params.user_id, function(err, user) {
+            if (err)
+                res.send(err);
+            res.json(user);
+        });
+    })
+
+    .put(function(req, res) {
+
+        // use our bear model to find the bear we want
+        User.findById(req.params.user_id, function(err, user) {
+
+            if (err)
+                res.send(err);
+
+            user.name = req.body.name;  // update the bears info
+
+            // save the bear
+            user.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'User with id=' + req.params.user_id + 'updated with name=' + user.name  });
+            });
+
+        });
+    });
+
+
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
+
+
 
 // START THE SERVER
 // =============================================================================

3d5e430d1c7c65a951d3edf772f0856205703a77
Sh4rp Fro5t <sh4rp.fro5t@gmail.com>
2015-11-17 17:38:54 +0100

    - Angular directives have now separate files - Added some doc

--- client/scripts/controllers/profilesSettings.js
+++ client/scripts/controllers/profilesSettings.js
@@ -5,55 +5,6 @@
  */
 angular.module('DomodiApp')
 
-    //TODO : See if directives can be exported in another file to be reused everywhere else
-    .directive('clockpicker', ['$timeout', '$parse', function ($timeout, $parse) {
-        return {
-            restrict: 'C',
-            require: 'ngModel',
-            link: function link(scope, elm, attrs, ngModel) {
-                var inputElm = $('input', elm);
-                var modelGetter = $parse(attrs['ngModel']);
-                var modelSetter = modelGetter.assign;
-
-                function afterUpdate() {
-                    return $timeout(function () {
-                        var value = modelGetter(scope);
-                        if (value) {
-                            value = moment(value);
-                        }
-                        var inputVal = moment(inputElm.val(), 'HH:mm');
-                        if (inputVal.isValid()) {
-                            if (!value) {
-                                modelSetter(scope, inputVal.toDate());
-                            } else {
-                                value.hour(inputVal.hour());
-                                value.minute(inputVal.minute());
-
-                                modelSetter(scope, value.toDate());
-
-                                scope.$digest();
-                            }
-                        }
-                    })
-                }
-
-                elm.clockpicker({
-                    donetext: 'Done',
-                    autoclose: true,
-                    afterDone: afterUpdate
-                });
-                inputElm.blur(afterUpdate);
-                ngModel.$formatters.push(function (value) {
-                    if (value) {
-                        inputElm.val(moment(value).format('HH:mm'));
-                    }
-                    return value;
-                });
-            }
-        }
-    }])
-
-
     .controller('ProfilesSettingsCtrl', function ($scope, domodiAPIservice) {
         // VARIABLES
         // =====================================================================
